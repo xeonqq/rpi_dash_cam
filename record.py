5ba72bc481d70c005856e16fb41ff1478752d8ef
xeonqq <xeonqq@gmail.com>
2021-03-18 20:52:28 +0100

    Run mp4 conversion in different thread to avoid blocking

--- record.py
+++ record.py
@@ -59,7 +59,37 @@ class Tape(object):
 
 
 class ToggleEvent(object):
-    pass
+    def __init__(self, camera_recorder):
+        self._camera_recorder = camera_recorder
+
+    def execute(self):
+        self._camera_recorder.toggle()
+
+
+class ConvertMP4Event(object):
+    def __init__(self, camera_recorder):
+        self._camera_recorder = camera_recorder
+        # copy once, since it can change in recorder
+        self._filename = self._camera_recorder.filename
+
+    def execute(self):
+        t = threading.Thread(target=self._convert_to_MP4)
+        t.start()
+
+    def _convert_to_MP4(self):
+        cmd = "MP4Box -add {filename}:fps={fps} {filename}.mp4".format(
+            filename=self._filename, fps=self._camera_recorder.fps
+        )
+        code = subprocess.call(cmd.split())
+        if code == 0:
+            self._delete_h264_file()
+            logging.info("converted h264 file to {}.mp4".format(self._filename))
+        else:
+            logging.error("conversion to mp4 failed")
+
+    def _delete_h264_file(self):
+        cmd = "rm {filename}".format(filename=self._filename)
+        subprocess.call(cmd.split())
 
 
 class CameraRecorder(StateMachine):
@@ -77,18 +107,28 @@ class CameraRecorder(StateMachine):
         self._led = led
         self._tape = Tape()
         self._events = queue.Queue()
+        self._conversion_queue = queue.Queue()
         self._button.add_pressed_cb(self._add_toggle_event)
+
+    @property
+    def fps(self):
+        return self._camera.framerate
+
+    @property
+    def filename(self):
+        return self._filename
 
     # async call from button interrupt
     def _add_toggle_event(self, pin):
-        self._events.put(ToggleEvent())
+        self._events.put(ToggleEvent(self))
+
+    def _add_convert_mp4_event(self):
+        self._events.put(ConvertMP4Event(self))
 
     def _process_event(self):
         if not self._events.empty():
-            if self._events.qsize() % 2 == 1:
-                with self._events.mutex:
-                    self._events.queue.clear()
-                self.toggle()
+            evt = self._events.get()
+            evt.execute()
 
     def on_enter_recording(self):
         self._filename = "{}/recording_{}.h264".format(
@@ -100,25 +140,10 @@ class CameraRecorder(StateMachine):
 
     def on_enter_idle(self):
         logging.info("stop recording...")
+        self._led.off()
         self._camera.stop_recording()
         self._tape.close()
-        self._led.off()
-        self._convert_record_to_mp4()
-
-    def _convert_record_to_mp4(self):
-        cmd = "MP4Box -add {filename}:fps={fps} {filename}.mp4".format(
-            filename=self._filename, fps=self._camera.framerate
-        )
-        code = subprocess.call(cmd.split())
-        if code == 0:
-            self._delete_h264_file()
-            logging.info("converted h264 file to {}.mp4".format(self._filename))
-        else:
-            logging.error("conversion to mp4 failed")
-
-    def _delete_h264_file(self):
-        cmd = "rm {filename}".format(filename=self._filename)
-        subprocess.call(cmd.split())
+        self._add_convert_mp4_event()
 
     def run(self):
         while True:
